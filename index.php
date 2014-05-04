32295c2b092fdff158bd9dda5e4bedcac68904d7
Agam Romdoni <agam.romdoni@student.unsil.ac.id>
2014-05-04 09:26:42 -0700

    a
    a

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
-<
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Tugas Layanan Web</title>
+ <link href="web.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="isi">
+ <div id="atas">
+  <div id="logo">
+  <img src="img/logo.png" alt="logo">
+  </div>
+  <div id="nav">
+  <ul class="nav-link">
+     <li id="acitve-nav"><a href="index.php">Home</a></li>
+     <li><a href="tugas.php.php">Tugas</a></li>
+     <li><a href="about.php.php">About</a></li>
+     <li><a href="contact.php.php">Contact</a></li>
+  </ul>
+  </div>
+ </div>
+ <div id="konten">
+ <img src="img/mhs.png" alt="mhs">
+ </div>
+ <div id="footer">
+  <div id="footer-link">
+  <a href="index.php">Home</a> | <a href="tugas.php">Tugas</a> | <a href="about.php">About</a> | <a href="contact.php">Contact</a>
+  </div>
+  <div id="footer-copy">
+  <p>&copy; Tugas Layanan Web E 2011</p>
+  </div>
+ </div>
+</div>
+
+</body>
+</html>
