65aef1618a867048d0048c84348c01d431aac1c6
Agam Romdoni <agam.romdoni@student.unsil.ac.id>
2014-05-04 09:27:29 -0700

    a
    a

--- oke zone1.php
+++ oke zone1.php
@@ -1,1 +1,18 @@
-a
+<?php
+$file ="http://techno.okezone.com/breaking/56";
+$doc = new DOMDocument();
+$doc->loadHTMLFile($file);
+
+$xpath = new DOMXpath($doc);
+$elements = $xpath->query("//*[@id]");
+if (!is_null($elements)) {
+  foreach ($elements as $element) {
+    echo "<br/>[". $element->nodeName. "]";
+
+    $nodes = $element->childNodes;
+    foreach ($nodes as $node) {
+      echo $node->nodeValue. "\n";
+    }
+  }
+}
+?>
